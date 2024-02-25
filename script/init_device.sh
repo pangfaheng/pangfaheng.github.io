e1710f9e295220e26d1c197c25416c4468194604
pangfaheng <pangfaheng@163.com>
2024-02-25 18:24:25 +0800

    1

--- script/init_device.sh
+++ script/init_device.sh
@@ -9,13 +9,15 @@ mount_disk=`sudo lsblk --output NAME,TYPE,UUID,MOUNTPOINT|awk 'NR != 1 && $2 != 
 # 获取未挂载的磁盘列表
 for device in `sudo lsblk --output NAME,TYPE,UUID,MOUNTPOINT|awk 'NR != 1 && $2 == "disk" && $3 == "" && $4 == "" {print $1}'`
 do
-    if [[ ! $mount_disk =~ $device ]];then
+    if [[ ! $mount_disk =~ $device ]]
+    then
         unmount_disks[${#unmount_disks[@]}]="/dev/$device"
     fi
 done
 
 # 判断是否有硬盘未挂载
-if [[ ${#unmount_disks[@]} -eq 0 ]];then
+if [[ ${#unmount_disks[@]} -eq 0 ]]
+then
     exit 0
 fi
 
@@ -28,7 +30,8 @@ done
 # 开机挂载硬盘
 for ((i=0; i<${#unmount_disks[@]}; i++))
 do
-    if [[ $i == 0 ]];then
+    if [[ $i == 0 ]]
+    then
         data_path='/data'
     else
         data_path="/data"`expr ${i} + 1`
@@ -43,7 +46,8 @@ done
 sudo mount -a
 exec_std=`echo $?`
 
-if [[ $exec_std -eq 0 ]];then
+if [[ $exec_std -eq 0 ]]
+then
     exit 0
 else
     echo "挂载磁盘失败，请检查服务器"
