3c9fe699ac739eac722e96f34e857db998b5c6d8
pangfaheng <pangfaheng@163.com>
2024-02-28 17:46:57 +0800

    1

--- script/time_format/time_format.py
+++ script/time_format/time_format.py
@@ -8,17 +8,19 @@ import pytz
 class TimeFormat:
     def __init__(
         self,
-        unix_timestamp=int(time.time()),
+        timestamp=int(time.time()),
         offset=None,
         timezone="+8",
         offset_timezone_unit="hour",
     ):
-        self.unix_timestamp = unix_timestamp
-        self.offset = self.setTimezoneOffset(
+        self.timestamp = timestamp
+        self.offset = self.set_timezone_offset(
             offset=offset, timezone=timezone, offset_timezone_unit=offset_timezone_unit
         )
 
-    def setTimezoneOffset(self, offset=None, timezone=None, offset_timezone_unit=None):
+    def set_timezone_offset(
+        self, offset=None, timezone=None, offset_timezone_unit=None
+    ):
         if offset:
             offset = offset
         elif timezone is not None and offset_timezone_unit == "hour":
@@ -26,10 +28,10 @@ class TimeFormat:
         elif timezone is not None and offset_timezone_unit == "min":
             offset = int(timezone)
         else:
-            offset = self.getServerTimezoneOffset()
+            offset = self.get_server_timezone_offset()
         return offset
 
-    def getServerTimezoneOffset(self):
+    def get_server_timezone_offset(self):
         # 获取当前服务器时间
         server_time = datetime.datetime.now(pytz.UTC)
         # 获取服务器时区
@@ -41,9 +43,9 @@ class TimeFormat:
         # 返回偏移量
         return offset_minutes
 
-    def generateOffsetUtcTime(self):
+    def generate_offset_utctime(self):
         # 转换为 UTC 时间
-        utc_time = datetime.datetime.utcfromtimestamp(self.unix_timestamp)
+        utc_time = datetime.datetime.utcfromtimestamp(self.timestamp)
         # 创建带偏移量的时区对象
         timezone = pytz.FixedOffset(self.offset)
         # 将 UTC 时间转换为带偏移量的时间
@@ -52,8 +54,22 @@ class TimeFormat:
         iso_format = offset_time.isoformat()
         return iso_format
 
+    def get_timpstamp(self):
+        return self.timestamp
+
+    def remake_iso8601_to_timestamp(self, iso_8601_time=None):
+        # 解析 ISO 8601 标准时间字符串
+        if not iso_8601_time:
+            dt = datetime.datetime.fromisoformat(self.generate_offset_utctime())
+        else:
+            dt = datetime.datetime.fromisoformat(iso_8601_time)
+        # 转换为时间戳（单位：秒）
+        timestamp = dt.timestamp()
+        return int(timestamp)
+
 
 if __name__ == "__main__":
     utc_time = TimeFormat()
-    utc_time_with_offset = utc_time.generateOffsetUtcTime()
-    print(utc_time_with_offset)
+    utc_time_with_offset = utc_time.generate_offset_utctime()
+    utc_time_with_timestamp = utc_time.remake_iso8601_to_timestamp()
+    print(utc_time_with_offset, utc_time_with_timestamp)
